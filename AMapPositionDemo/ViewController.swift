6cdf41e6fa57fda8aa33425ceec6c256eedbc197
lfb <962429707@qq.com>
2016-11-25 14:19:23 +0800

    创建工程

---
+++ AMapPositionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AMapPositionDemo
+//
+//  Created by lifubing on 16/11/25.
+//  Copyright © 2016年 lifubing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
